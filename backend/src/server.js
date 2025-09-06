3e9bc4b0a1d7428b75755f43228423cc431c070b
Jnaneswar <for100ever4@gmail.com>
2025-09-06 13:14:30 +0530

    stream and sentry added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,42 @@
+import '../instrument.mjs';
+import dotenv from 'dotenv';  
+import * as Sentry from "@sentry/node";
+
 import express from 'express';
-import dotenv from 'dotenv';
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
 import { clerkMiddleware } from '@clerk/express';
 import { inngest, functions } from './config/inngest.js';
 import { serve } from 'inngest/express';
+import chatRoutes from "./routes/chat.route.js";
+
+
 dotenv.config();
 
 const app = express();
 
+
+
 app.use(express.json());
 app.use(clerkMiddleware());//req.auth is available in the request object
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.use(express.urlencoded({ extended: true }));
+app.get("/debug-sentry", (req, res) => {
+  throw new Error("My first Sentry error!");
+});
+
+// app.use(express.urlencoded({ extended: true }));
+
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+  res.send('Hello World! 123');
 });
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+
+Sentry.setupExpressErrorHandler(app);
 
 // Health check endpoint
 app.get('/health', (req, res) => {
