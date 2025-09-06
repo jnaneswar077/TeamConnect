e33a170060e987eb5477be94620e4bbe2e390c73
Jnaneswar <for100ever4@gmail.com>
2025-09-06 15:00:35 +0530

    auth page implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
+import { Route, Routes, Navigate } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AuthPage from './pages/AuthPage';
 const App = () => {
   return (
-  <header>
-  <SignedOut>
-    <SignInButton mode='modal'/>
-  </SignedOut>
-  <SignedIn>
-    <UserButton />
-  </SignedIn>
-</header>);
+  <>
+
+    <SignedIn>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<Navigate to={"/"} replace />} />
+        </Routes>
+    </SignedIn>
+
+    <SignedOut>
+      <Routes>
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="*" element={<Navigate to={"/auth"} replace />} />
+      </Routes>
+    </SignedOut>
+    
+  </>);
 }
 
 export default App
